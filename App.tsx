539734fef32308de6ada277185a4eafd911ad86d
kamabokochan <h.sasaki.worksfront@gmail.com>
2021-06-04 23:56:00 +0900

    メッセージのフォームUI作成

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  Pressable,
+  Alert,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputWrap}>
+        <TextInput
+          style={styles.input}
+          // onChangeText={onChangeNumber}
+          // value={number}
+          placeholder="メッセージを入力してください"
+          keyboardType="numeric"
+        />
+        <Pressable
+          style={styles.sendButton}
+          onPress={() => Alert.alert("Simple Button pressed")}
+        >
+          <Text>送信</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -14,8 +33,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputWrap: {
+    flexDirection: "row",
+    // alignItems: "center",
+  },
+  input: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 4,
+    borderColor: "#ccc",
+    borderWidth: 1,
+  },
+  sendButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 4,
+    borderColor: "#ccc",
+    borderWidth: 1,
   },
 });
